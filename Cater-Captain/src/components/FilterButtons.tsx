28ba6111f3532ab6cf5a7d3a31ccb08c3b654ea1
JayEye <136415777+JayEyeM@users.noreply.github.com>
2024-05-31 23:27:20 -0230

    using DayJS for filter buttons logic.
    other minor changes as well. Event id glitch fix. Doubles of initial events glitch fix.

--- Cater-Captain/src/components/FilterButtons.tsx
+++ Cater-Captain/src/components/FilterButtons.tsx
@@ -2,46 +2,77 @@ import React from 'react';
 import { Stack } from '@chakra-ui/react';
 import { Event } from './Interfaces';
 import { SolidLightBlueButton } from './Buttons';
+import dayjs from 'dayjs';
 
-//filter events buttons for dashboard
-
-//props for filter buttons
 interface FilterButtonsProps {
-    setFilteredEvents: React.Dispatch<React.SetStateAction<Event[]>>;
-    savedEvents: Event[];
-    handleThisWeek: () => void;
-    handleThisMonth: () => void;
-    handleAllEvents: () => void;
-  }
-  
-  
-//filter functions
+  setFilteredEvents: React.Dispatch<React.SetStateAction<Event[]>>;
+  savedEvents: Event[];
+}
+
 const FilterButtons: React.FC<FilterButtonsProps> = ({ setFilteredEvents, savedEvents }) => {
-//button for current week
+  const getCurrentDay = () => {
+    return dayjs();
+  };
+
+  const getCurrentWeek = () => {
+    const today = dayjs();
+    const startOfWeek = today.startOf('week');
+    const endOfWeek = today.endOf('week');
+    return {
+      start: startOfWeek.format('YYYY-MM-DD'),
+      end: endOfWeek.format('YYYY-MM-DD')
+    };
+  };
+
+  const getCurrentMonth = () => {
+    const today = dayjs();
+    const startOfMonth = today.startOf('month');
+    const endOfMonth = today.endOf('month');
+    return {
+      start: startOfMonth.format('YYYY-MM-DD'),
+      end: endOfMonth.format('YYYY-MM-DD')
+    };
+  };
+
+  const getNextMonth = () => {
+    const today = dayjs();
+    const startOfNextMonth = today.add(1, 'month').startOf('month');
+    const endOfNextMonth = today.add(1, 'month').endOf('month');
+    return {
+      start: startOfNextMonth.format('YYYY-MM-DD'),
+      end: endOfNextMonth.format('YYYY-MM-DD')
+    };
+  };
+
+  const handleToday = () => {
+    const currentDay = getCurrentDay().format('YYYY-MM-DD');
+    handleFilterChange(currentDay, currentDay);
+  };
+
   const handleThisWeek = () => {
-    const now = new Date();
-    const startOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
-    const endOfWeek = new Date(now.setDate(now.getDate() - now.getDay() + 6));
-    handleFilterChange(startOfWeek, endOfWeek);
+    const currentWeek = getCurrentWeek();
+    handleFilterChange(currentWeek.start, currentWeek.end);
   };
 
-  //button for current month
   const handleThisMonth = () => {
-    const now = new Date();
-    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-    const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-    handleFilterChange(startOfMonth, endOfMonth);
+    const currentMonth = getCurrentMonth();
+    handleFilterChange(currentMonth.start, currentMonth.end);
   };
 
-  //all events
+  const handleNextMonth = () => {
+    const nextMonth = getNextMonth();
+    handleFilterChange(nextMonth.start, nextMonth.end);
+  };
+
   const handleAllEvents = () => {
-    setFilteredEvents(savedEvents);
+    const startOfAllEvents = '1970-01-01';
+    const endOfAllEvents = '2038-01-01';
+    handleFilterChange(startOfAllEvents, endOfAllEvents);
   };
 
-  //filter change to set filtered events
-  const handleFilterChange = (startDate: Date, endDate: Date) => {
-    const filteredEvents = savedEvents.filter(event => {
-      const eventDate = new Date(event.EventDate);
+  const handleFilterChange = (startDate: string, endDate: string) => {
+    const filteredEvents = savedEvents.filter((event) => {
+      const eventDate = dayjs(event.EventDate).format('YYYY-MM-DD');
       return eventDate >= startDate && eventDate <= endDate;
     });
     setFilteredEvents(filteredEvents);
@@ -49,8 +80,10 @@ const FilterButtons: React.FC<FilterButtonsProps> = ({ setFilteredEvents, savedE
 
   return (
     <Stack direction="row" spacing={4} mb={4}>
+      <SolidLightBlueButton onClick={handleToday}>Today</SolidLightBlueButton>
       <SolidLightBlueButton onClick={handleThisWeek}>This Week</SolidLightBlueButton>
       <SolidLightBlueButton onClick={handleThisMonth}>This Month</SolidLightBlueButton>
+      <SolidLightBlueButton onClick={handleNextMonth}>Next Month</SolidLightBlueButton>
       <SolidLightBlueButton onClick={handleAllEvents}>All Events</SolidLightBlueButton>
     </Stack>
   );
